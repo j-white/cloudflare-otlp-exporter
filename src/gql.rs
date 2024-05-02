8a87c9d87c796f346e233c9eb4f3c1c9c989c90b
Jesse White <jesse.white.oss@gmail.com>
2024-05-02 18:46:09 -0400

    make a code path down to the subrequests

--- src/gql.rs
+++ src/gql.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use graphql_client::GraphQLQuery;
 
 // The paths are relative to the directory where your `Cargo.toml` is located.
@@ -6,8 +7,9 @@ use graphql_client::GraphQLQuery;
 #[graphql(
 schema_path = "gql/schema.graphql",
 query_path = "gql/queries.graphql",
+response_derives = "Debug,Clone"
 )]
-pub struct GetWorkersAnalytics;
+pub struct GetWorkersAnalyticsQuery;
 
 #[allow(non_camel_case_types)]
 type float32 = f32;
@@ -20,3 +22,14 @@ type Time = String;
 
 #[allow(non_camel_case_types)]
 type uint64 = u64;
+
+pub async fn perform_my_query(variables: get_workers_analytics_query::Variables) -> Result<(), Box<dyn Error>> {
+    let request_body = GetWorkersAnalyticsQuery::build_query(variables);
+    let client = reqwest::Client::new();
+    let res = client.post("/graphql").json(&request_body).send().await?;
+    let response: get_workers_analytics_query::ResponseData = res.json().await?;
+    let _ = response.viewer.unwrap().accounts.iter().map(|account| account.workers_invocations_adaptive.iter().map(|worker| {
+        return worker.sum.as_ref().unwrap().subrequests;
+    }));
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,12 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    let variables = gql::get_workers_analytics_query::Variables {
+        account_tag: Some("".to_string()),
+        datetime_start: Some("".to_string()),
+        datetime_end: Some("".to_string()),
+        script_name: Some("".to_string()),
+    };
+    let _ = gql::perform_my_query(variables);
     Response::ok("Hello, World!")
 }
